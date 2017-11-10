7e08a027063a5eb6aa0fcf93c05d720874847e30
Bruno Turcksin <bruno.turcksin@gmail.com>
2017-11-10 11:02:18 -0500

    Add continuous integration.

---
+++ ci/compile_and_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd $1
+rm -rf build
+mkdir build && cd build
+cmake \
+  -D CMAKE_BUILD_TYPE=Debug \
+  -D MFMG_ENABLE_TESTS=ON \
+  -D BUILD_SHARED_LIBS=ON \
+  -D MFMG_ENABLE_ClangFormat=ON \
+  -D MFMG_ENABLE_COVERAGE=ON \
+  -D MFMG_ENABLE_DOCUMENTATION=OFF \
+../
+make -j12
+ctest -j12 --no-compress-output -T Test
+
+# Code coverage
+lcov --capture --directory mfmg --output-file lcov.info
+curl -s https://codecov.io/bash -o codecov_bash_uploader
+chmod +x codecov_bash_uploader
+./codecov_bash_uploader -Z -X gcov -f lcov.info
+
+exit 0
