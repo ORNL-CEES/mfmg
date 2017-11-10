bc21ff112790ba33d8ff74257f90ca2fed18933b
Bruno Turcksin <bruno.turcksin@gmail.com>
2017-11-10 16:20:33 -0500

    Update license.

--- tests/test_dummy.cc
+++ tests/test_dummy.cc
@@ -5,6 +5,8 @@
  * This file is part of the mfmg libary. mfmg is distributed under a BSD *
  * 3-clause license. For the licensing terms see the LICENSE file in the *
  * top-level directory                                                   *
+ *                                                                       *
+ * SPDX-License-Identifier: BSD-3-Clause                                 *
  *************************************************************************/
 
 #define BOOST_TEST_MODULE dummy
