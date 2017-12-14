a81a2123ef7fe49017e3858f766247f447da89b4
Bruno Turcksin <bruno.turcksin@gmail.com>
2017-12-14 09:15:08 -0500

    Add -DDEBUG when deal.II is compiled in debug mode
    Fix a bug in cmake where -DDEBUG was not used when deal.II was compiled in
    debug mode. This prevented dealii::Assert to be enabled in header files.

--- cmake/SetupTPLs.cmake
+++ cmake/SetupTPLs.cmake
@@ -16,12 +16,19 @@ FIND_PACKAGE(deal.II 8.5 REQUIRED PATHS ${DEAL_II_DIR})
 
 # If deal.II was configured in DebugRelease mode, then if mfmg was configured
 # in Debug mode, we link against the Debug version of deal.II. If mfmg was
-# configured in Release mode, we link against the Release version of eal.II
+# configured in Release mode, we link against the Release version of deal.II. If
+# we configure with the Debug version of deal.II, we need to use -DDEBUG to
+# enable dealii::Assert in header files.
 STRING(FIND "${DEAL_II_LIBRARIES}" "general" SINGLE_DEAL_II)
 IF (${SINGLE_DEAL_II} EQUAL -1)
   IF(CMAKE_BUILD_TYPE MATCHES "Release")
     SET(DEAL_II_LIBRARIES ${DEAL_II_LIBRARIES_RELEASE})
   ELSE()
     SET(DEAL_II_LIBRARIES ${DEAL_II_LIBRARIES_DEBUG})
+    ADD_DEFINITIONS(-DDEBUG)
+  ENDIF()
+ELSE()
+  IF (NOT "${DEAL_II_TARGET_DEBUG}" STREQUAL "")
+    ADD_DEFINITIONS(-DDEBUG)
   ENDIF()
 ENDIF()
