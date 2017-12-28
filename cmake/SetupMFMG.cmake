0cb0da12c5c6f834e2ff81275b5caa986d856c9f
Bruno Turcksin <bruno.turcksin@gmail.com>
2017-12-28 09:22:34 -0500

    Do not set BOOST_DISABLE_ASSERTS since we don't use BOOST_ASSERT

--- cmake/SetupMFMG.cmake
+++ cmake/SetupMFMG.cmake
@@ -13,11 +13,11 @@ SET(CMAKE_CXX_FLAGS_DEBUG "-g")
 # Disable boost asserts in Release mode and set MFMG_DEBUG in Debug mode
 MESSAGE("-- Build type: ${CMAKE_BUILD_TYPE}")
 IF(CMAKE_BUILD_TYPE MATCHES "Release" OR CMAKE_BUILD_TYPE MATCHES "RelWithDebInfo")
-  ADD_DEFINITIONS(-DBOOST_DISABLE_ASSERTS)
+  # Do nothing
 ELSEIF(CMAKE_BUILD_TYPE MATCHES "Debug")
   ADD_DEFINITIONS(-DMFMG_DEBUG)
 ELSE()
   MESSAGE(SEND_ERROR
-    "Possible values for CMAKE_BUILD_TYPE are Release, RelWithDebInfo and Debug"
+    "Possible values for CMAKE_BUILD_TYPE are Release, RelWithDebInfo, and Debug"
     )
 ENDIF()
