3fee707e20537ab22ac0642f72e3553f2811fc6e
Bruno Turcksin <bruno.turcksin@gmail.com>
2018-04-05 14:52:55 -0400

    Reduce tolerance when testing the hierarchy on hyper_ball

--- tests/test_hierarchy.cc
+++ tests/test_hierarchy.cc
@@ -356,7 +356,8 @@ BOOST_DATA_TEST_CASE(hierarchy_3d,
       // TODO investigate why on the hyper_ball the error is larger on the
       // testing machine
       BOOST_TEST(
-          conv_rate <
-          2 * ref_solution[std::make_tuple(mesh, distort_random, reordering)]);
+          conv_rate ==
+              ref_solution[std::make_tuple(mesh, distort_random, reordering)],
+          tt::tolerance(1e-6));
   }
 }
