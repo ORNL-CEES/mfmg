7bd9ad73ec1a7ea9f7c9dff60fa8f362590bd9a8
Bruno Turcksin <bruno.turcksin@gmail.com>
2018-05-14 14:11:42 -0400

    Add functions to sort a vector given multiple key vectors

--- tests/test_utils.cc
+++ tests/test_utils.cc
@@ -59,3 +59,28 @@ coarse
   BOOST_TEST(plist.sublist("coarse").sublist("params").get<int>("verbosity") ==
              5);
 }
+
+BOOST_AUTO_TEST_CASE(sorting)
+{
+  unsigned int const size = 10;
+  std::vector<int> vec_1(size);
+  std::vector<int> vec_2(size);
+
+  for (unsigned int i = 0; i < size; ++i)
+  {
+    vec_1[i] = i % 2;
+    vec_2[i] = i;
+  }
+
+  auto permutation = mfmg::sort_permutation(vec_1, vec_2);
+
+  mfmg::apply_permutation_in_place(permutation, vec_2);
+  std::vector<int> reference(size);
+  for (unsigned int i = 0; i < size / 2; ++i)
+  {
+    reference[i] = 2 * i;
+    reference[i + size / 2] = 2 * i + 1;
+  }
+
+  BOOST_TEST(reference == vec_2);
+}
