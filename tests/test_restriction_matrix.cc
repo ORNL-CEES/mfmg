c7ff33db3b900674383fb4825ed3b223a0f1140f
Damien L-G <dalg24@gmail.com>
2018-11-15 15:20:58 -0500

    Update test_restriction_matrix.cc to pass diagonal instead of the system matrix

--- tests/test_restriction_matrix.cc
+++ tests/test_restriction_matrix.cc
@@ -309,9 +309,28 @@ BOOST_AUTO_TEST_CASE(weight_sum, *utf::tolerance(1e-4))
                                    laplace._system_matrix);
   mfmg::AMGe_host<dim, MeshEvaluator, DVector> amge(comm, laplace._dof_handler);
 
+  // FIXME need a function to extract the diag
+  auto const &dof_handler = laplace._dof_handler;
+  auto const &system_sparse_matrix = laplace._system_matrix;
+  dealii::IndexSet locally_owned_dofs =
+      system_sparse_matrix.locally_owned_domain_indices();
+  dealii::IndexSet locally_relevant_dofs;
+  dealii::DoFTools::extract_locally_relevant_dofs(dof_handler,
+                                                  locally_relevant_dofs);
+  dealii::LinearAlgebra::distributed::Vector<double> locally_owned_global_diag(
+      locally_owned_dofs, comm);
+  for (auto const val : locally_owned_dofs)
+    locally_owned_global_diag[val] = system_sparse_matrix.diag_element(val);
+  locally_owned_global_diag.compress(dealii::VectorOperation::insert);
+
+  dealii::LinearAlgebra::distributed::Vector<double>
+      locally_relevant_global_diag(locally_owned_dofs, locally_relevant_dofs,
+                                   comm);
+  locally_relevant_global_diag = locally_owned_global_diag;
+
   dealii::TrilinosWrappers::SparseMatrix restrictor_matrix;
   amge.setup_restrictor(agglomerate_ptree, n_eigenvectors, tolerance, evaluator,
-                        laplace._system_matrix, restrictor_matrix);
+                        locally_relevant_global_diag, restrictor_matrix);
 
   // Multiply the matrix by three because all the eigenvectors are 1/3. So we
   // are left with the weights.
