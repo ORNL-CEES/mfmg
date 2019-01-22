36ac4e28e7f1587067f25cb08c7eb0e36de859bb
Bruno Turcksin <bruno.turcksin@gmail.com>
2019-01-22 17:04:10 -0500

    Fix typo in copyright

--- include/mfmg/common/hierarchy.hpp
+++ include/mfmg/common/hierarchy.hpp
@@ -1,13 +1,13 @@
-/**************************************************************************
- * Copyright (c) 2017-2019 by the mfmg authors                            *
- * All rights reserved.                                                   *
- *                                                                        *
- * This file is part of the mfmg libary. mfmg is distributed under op BSD *
- * 3-clause license. For the licensing terms see the LICENSE file in the  *
- * top-level directory                                                    *
- *                                                                        *
- * SPDX-License-Identifier: BSD-3-Clause                                  *
- **************************************************************************/
+/*************************************************************************
+ * Copyright (c) 2017-2019 by the mfmg authors                           *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * This file is part of the mfmg libary. mfmg is distributed under a BSD *
+ * 3-clause license. For the licensing terms see the LICENSE file in the *
+ * top-level directory                                                   *
+ *                                                                       *
+ * SPDX-License-Identifier: BSD-3-Clause                                 *
+ *************************************************************************/
 
 #ifndef MFMG_HIERARCHY_HPP
 #define MFMG_HIERARCHY_HPP
