40ac016529879421ea2d5d574694c06de1041ac7
Bruno Turcksin <bruno.turcksin@gmail.com>
2019-02-05 12:21:13 -0500

    Add cuda_matrix_free_mesh_evaluator

---
+++ include/mfmg/cuda/cuda_matrix_free_mesh_evaluator.cuh
@@ -0,0 +1,44 @@
+/*************************************************************************
+ * Copyright (c) 2017-2019 by the mfmg authors                           *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * This file is part of the mfmg libary. mfmg is distributed under a BSD *
+ * 3-clause license. For the licensing terms see the LICENSE file in the *
+ * top-level directory                                                   *
+ *                                                                       *
+ * SPDX-License-Identifier: BSD-3-Clause                                 *
+ *************************************************************************/
+
+#ifndef MFMG_CUDA_MATRIX_FREE_MESH_EVALUATOR_CUH
+#define MFMG_CUDA_MATRIX_FREE_MESH_EVALUATOR_CUH
+
+#ifdef MFMG_WITH_CUDA
+
+#include <mfmg/cuda/cuda_mesh_evaluator.cuh>
+
+namespace mfmg
+{
+template <int dim>
+class CudaMatrixFreeMeshEvaluator : public CudaMeshEvaluator<dim>
+{
+public:
+  CudaMatrixFreeMeshEvaluator(CudaHandle const &cuda_handle,
+                              dealii::DoFHandler<dim> &dof_handler,
+                              dealii::AffineConstraints<double> &constraints)
+      : CudaMeshEvaluator<dim>(cuda_handle, dof_handler, constraints)
+  {
+  }
+
+  void apply(VectorDevice<double> const &src, VectorDevice<double> &dst) const;
+
+  void apply(dealii::LinearAlgebra::distributed::Vector<double> const &src,
+             dealii::LinearAlgebra::distributed::Vector<double> &dst) const;
+
+  std::shared_ptr<dealii::LinearAlgebra::distributed::Vector<double>>
+  build_range_vector() const;
+};
+} // namespace mfmg
+
+#endif
+
+#endif
---
+++ source/cuda/cuda_matrix_free_mesh_evaluator.cu
@@ -0,0 +1,57 @@
+/*************************************************************************
+ * Copyright (c) 2017-2019 by the mfmg authors                           *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * This file is part of the mfmg libary. mfmg is distributed under a BSD *
+ * 3-clause license. For the licensing terms see the LICENSE file in the *
+ * top-level directory                                                   *
+ *                                                                       *
+ * SPDX-License-Identifier: BSD-3-Clause                                 *
+ *************************************************************************/
+
+#include <mfmg/cuda/cuda_matrix_free_mesh_evaluator.cuh>
+
+namespace mfmg
+{
+template <int dim>
+std::shared_ptr<dealii::LinearAlgebra::distributed::Vector<double>>
+CudaMatrixFreeMeshEvaluator<dim>::build_range_vector() const
+{
+
+  // Get the MPI communicator from the dof handler
+  auto const &triangulation = (this->_dof_handler).get_triangulation();
+  using Triangulation =
+      typename std::remove_reference<decltype(triangulation)>::type;
+  auto comm = static_cast<dealii::parallel::Triangulation<
+      Triangulation::dimension, Triangulation::space_dimension> const &>(
+                  triangulation)
+                  .get_communicator();
+
+  // Get the set of locally owned DoFs
+  auto const &locally_owned_dofs = (this->_dof_handler).locally_owned_dofs();
+
+  return std::make_shared<dealii::LinearAlgebra::distributed::Vector<double>>(
+      locally_owned_dofs, comm);
+}
+
+template <int dim>
+void CudaMatrixFreeMeshEvaluator<dim>::apply(VectorDevice<double> const &src,
+                                             VectorDevice<double> &dst) const
+{
+  // TODO
+  ASSERT_THROW_NOT_IMPLEMENTED();
+}
+
+template <int dim>
+void CudaMatrixFreeMeshEvaluator<dim>::apply(
+    dealii::LinearAlgebra::distributed::Vector<double> const &src,
+    dealii::LinearAlgebra::distributed::Vector<double> &dst) const
+{
+  // TODO
+  ASSERT_THROW_NOT_IMPLEMENTED();
+}
+} // namespace mfmg
+
+// Explicit Instantiation
+template class mfmg::CudaMatrixFreeMeshEvaluator<2>;
+template class mfmg::CudaMatrixFreeMeshEvaluator<3>;
