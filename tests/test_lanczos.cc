5ae8a356fd8d24536893c1152219431a65b21e0b
Andrey Prokopenko <prokopenkoav@ornl.gov>
2019-02-12 14:20:02 -0500

    Upgrade Lanczos test to test both deflated and regular

--- tests/test_lanczos.cc
+++ tests/test_lanczos.cc
@@ -13,15 +13,19 @@
 
 #include "mfmg/dealii/lanczos.templates.hpp"
 
+#include <boost/test/data/test_case.hpp>
+
 #include <cmath>
 #include <cstdio>
 
 #include "lanczos_simpleop.templates.hpp"
 #include "main.cc"
 
+namespace bdata = boost::unit_test::data;
 namespace tt = boost::test_tools;
 
-BOOST_AUTO_TEST_CASE(lanczos)
+BOOST_DATA_TEST_CASE(lanczos, bdata::make({1, 2}) * bdata::make({false, true}),
+                     multiplicity, is_deflated)
 {
   using namespace mfmg;
 
@@ -29,14 +33,27 @@ BOOST_AUTO_TEST_CASE(lanczos)
   using OperatorType = SimpleOperator<VectorType>;
 
   const int n = 1000;
-  const int multiplicity = 2;
+  const int n_distinct_eigenvalues = 3;
+  const int n_eigenvectors = n_distinct_eigenvalues * multiplicity;
 
   OperatorType op(n, multiplicity);
 
   boost::property_tree::ptree lanczos_params;
-  lanczos_params.put("is_deflated", true);
-  lanczos_params.put("num_eigenpairs_per_cycle", 2);
-  lanczos_params.put("num_cycles", 3);
+  lanczos_params.put("is_deflated", is_deflated);
+  if (is_deflated)
+  {
+    lanczos_params.put("num_cycles", multiplicity);
+    lanczos_params.put("num_eigenpairs_per_cycle", n_distinct_eigenvalues);
+  }
+  else
+  {
+    if (multiplicity > 1)
+    {
+      // Skip test when using regular Lanczos for multiplicities larger than 1
+      return;
+    }
+    lanczos_params.put("num_eigenpairs", n_eigenvectors);
+  }
   lanczos_params.put("max_iterations", 200);
   lanczos_params.put("tolerance", 1e-2);
   lanczos_params.put("percent_overshoot", 5);
@@ -45,9 +62,10 @@ BOOST_AUTO_TEST_CASE(lanczos)
 
   solver.solve();
 
-  const int n_eigenvectors = solver.num_evecs();
   auto ref_evals = op.get_evals();
   auto computed_evals = solver.get_evals();
+
+  BOOST_TEST(computed_evals.size() == n_eigenvectors);
 
   // Loop to ensure each Ritz value is near an eigenvalue.
   // TODO: make a more complete check of correctness of result
@@ -58,9 +76,5 @@ BOOST_AUTO_TEST_CASE(lanczos)
   std::sort(computed_evals.begin(), computed_evals.end());
 
   for (int i = 0; i < n_eigenvectors; i++)
-  {
-    std::cout << i << ": " << computed_evals[i] << ", " << ref_evals[i]
-              << std::endl;
     BOOST_TEST(computed_evals[i] == ref_evals[i], tt::tolerance(tolerance));
-  }
 }
