d50b346de5a91b11f3364258055fa1124b8065d6
Damien L-G <dalg24@gmail.com>
2019-03-07 10:47:21 -0500

    Add final specifier to DealIIHierarchyHelpers member functions

--- include/mfmg/dealii/dealii_hierarchy_helpers.hpp
+++ include/mfmg/dealii/dealii_hierarchy_helpers.hpp
@@ -22,16 +22,16 @@ class DealIIHierarchyHelpers : public HierarchyHelpers<VectorType>
 public:
   using vector_type = VectorType;
 
-  std::shared_ptr<Operator<vector_type>>
-  get_global_operator(std::shared_ptr<MeshEvaluator> mesh_evaluator) override;
+  std::shared_ptr<Operator<vector_type>> get_global_operator(
+      std::shared_ptr<MeshEvaluator> mesh_evaluator) override final;
 
   std::shared_ptr<Operator<vector_type>> build_restrictor(
       MPI_Comm comm, std::shared_ptr<MeshEvaluator> mesh_evaluator,
-      std::shared_ptr<boost::property_tree::ptree const> params) override;
+      std::shared_ptr<boost::property_tree::ptree const> params) override final;
 
   std::shared_ptr<Smoother<vector_type>> build_smoother(
       std::shared_ptr<Operator<vector_type> const> op,
-      std::shared_ptr<boost::property_tree::ptree const> params) override;
+      std::shared_ptr<boost::property_tree::ptree const> params) override final;
 
   std::shared_ptr<Solver<vector_type>> build_coarse_solver(
       std::shared_ptr<Operator<vector_type> const> op,
