0368f2095fbf60fca66ccab4238fd89546f08441
Daniel Arndt <arndtd@ornl.gov>
2019-04-03 17:58:24 -0400

    Allow finding TPLs by environment variables

--- cmake/SetupTPLs.cmake
+++ cmake/SetupTPLs.cmake
@@ -12,7 +12,7 @@ SET(Boost_COMPONENTS
 FIND_PACKAGE(Boost 1.65.1 REQUIRED COMPONENTS ${Boost_COMPONENTS})
 
 #### deal.II #################################################################
-FIND_PACKAGE(deal.II 9.0 REQUIRED PATHS ${DEAL_II_DIR})
+FIND_PACKAGE(deal.II 9.0 REQUIRED PATHS ${DEAL_II_DIR} $ENV{DEAL_II_DIR})
 
 # If deal.II was configured in DebugRelease mode, then if mfmg was configured
 # in Debug mode, we link against the Debug version of deal.II. If mfmg was
@@ -36,12 +36,23 @@ ENDIF()
 #### AMGX ####################################################################
 IF(${MFMG_ENABLE_CUDA})
   IF (${MFMG_ENABLE_AMGX})
+    FIND_LIBRARY(AMGX_LIBRARY NAMES amgx
+      HINTS ${AMGX_DIR} $ENV{AMGX_DIR}
+      PATH_SUFFIXES lib)
+    FIND_PATH(AMGX_INCLUDE_DIR amgx_c.h
+      HINTS ${AMGX_DIR} $ENV{AMGX_DIR}
+      PATH_SUFFIXES include)
+
+    IF(NOT AMGX_INCLUDE_DIR OR NOT AMGX_LIBRARY)
+      MESSAGE(FATAL_ERROR 
+              "Error! Could not locate AmgX.")
+    ENDIF()
+
     ADD_LIBRARY(amgx STATIC IMPORTED)
     SET_TARGET_PROPERTIES(amgx PROPERTIES IMPORTED_LOCATION
-      "${AMGX_DIR}/lib/libamgx.a"
+      ${AMGX_LIBRARY}
       IMPORTED_LINK_INTERFACE_LIBRARIES "cublas")
     ADD_DEFINITIONS(-DMFMG_WITH_AMGX)
-    SET(AMGX_INCLUDE_DIR "${AMGX_DIR}/include")
     SET(AMGX_LIBRARY amgx)
   ENDIF()
 ENDIF()
