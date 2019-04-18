715652e37f8a07d9123fc8db8624505ff2eae198
Bruno Turcksin <bruno.turcksin@gmail.com>
2019-04-18 14:51:55 +0000

    Use matrix-free MeshEvaluator in the tests

--- tests/test_hierarchy.cc
+++ tests/test_hierarchy.cc
@@ -32,11 +32,87 @@
 #include <random>
 
 #include "laplace.hpp"
+#include "laplace_matrix_free.hpp"
 #include "main.cc"
 #include "test_hierarchy_helpers.hpp"
 
 namespace bdata = boost::unit_test::data;
 namespace tt = boost::test_tools;
+
+template <typename MeshEvaluator>
+double test_mf(std::shared_ptr<boost::property_tree::ptree> params)
+{
+  using DVector = dealii::LinearAlgebra::distributed::Vector<double>;
+  int constexpr dim = MeshEvaluator::_dim;
+
+  MPI_Comm comm = MPI_COMM_WORLD;
+
+  dealii::ConditionalOStream pcout(
+      std::cout, dealii::Utilities::MPI::this_mpi_process(comm) == 0);
+
+  auto material_property =
+      MaterialPropertyFactory<dim>::create_material_property(
+          params->get<std::string>("material_property.type"));
+  Source<dim> source;
+
+  auto laplace_ptree = params->get_child("laplace");
+  int constexpr fe_degree = 1;
+  LaplaceMatrixFree<dim, fe_degree, double> mf_laplace(comm);
+  mf_laplace.setup_system(laplace_ptree, *material_property);
+
+  auto const locally_owned_dofs = mf_laplace._locally_owned_dofs;
+  DVector solution(locally_owned_dofs, comm);
+  DVector rhs(mf_laplace._system_rhs);
+
+  std::default_random_engine generator;
+  std::uniform_real_distribution<typename DVector::value_type> distribution(0.,
+                                                                            1.);
+  for (auto const index : locally_owned_dofs)
+  {
+    // Make the solution satisfy the Dirichlet conditions because these should
+    // be treated outside the preconditioner
+    if (mf_laplace._constraints.is_constrained(index))
+      solution[index] = 0.;
+    else
+      solution[index] = distribution(generator);
+  }
+
+  auto evaluator =
+      std::make_shared<TestMFMeshEvaluator<dim, fe_degree, double>>(
+          mf_laplace._dof_handler, mf_laplace._constraints,
+          mf_laplace._laplace_operator, material_property);
+  mfmg::Hierarchy<DVector> hierarchy(comm, evaluator, params);
+
+  // We want to do 20 V-cycle iterations. The rhs of is zero.
+  // Use D(istributed)Vector because deal has its own Vector class
+  DVector residual(rhs);
+  unsigned int const n_cycles = 20;
+  std::vector<double> res(n_cycles + 1);
+
+  mf_laplace._laplace_operator.vmult(residual, solution);
+  residual.sadd(-1., 1., rhs);
+  auto const residual0_norm = residual.l2_norm();
+
+  std::cout << std::scientific;
+  // pcout << "#0: " << 1.0 << std::endl;
+  res[0] = 1.0;
+  for (unsigned int i = 0; i < n_cycles; ++i)
+  {
+    hierarchy.apply(rhs, solution);
+
+    mf_laplace._laplace_operator.vmult(residual, solution);
+    residual.sadd(-1., 1., rhs);
+    double rel_residual = residual.l2_norm() / residual0_norm;
+    // pcout << "#" << i + 1 << ": " << rel_residual << std::endl;
+    res[i + 1] = rel_residual;
+  }
+
+  double const conv_rate = res[n_cycles] / res[n_cycles - 1];
+  pcout << "Convergence rate: " << std::fixed << std::setprecision(2)
+        << conv_rate << std::endl;
+
+  return conv_rate;
+}
 
 template <typename MeshEvaluator>
 double test(std::shared_ptr<boost::property_tree::ptree> params)
@@ -70,7 +146,14 @@ double test(std::shared_ptr<boost::property_tree::ptree> params)
   std::uniform_real_distribution<typename DVector::value_type> distribution(0.,
                                                                             1.);
   for (auto const index : locally_owned_dofs)
-    solution[index] = distribution(generator);
+  {
+    // Make the solution satisfy the Dirichlet conditions because these should
+    // be treated outside the preconditioner
+    if (laplace._constraints.is_constrained(index))
+      solution[index] = 0.;
+    else
+      solution[index] = distribution(generator);
+  }
 
   auto evaluator = std::make_shared<TestMeshEvaluator<MeshEvaluator>>(
       laplace._dof_handler, laplace._constraints, fe_degree, a,
@@ -108,44 +191,54 @@ double test(std::shared_ptr<boost::property_tree::ptree> params)
   return conv_rate;
 }
 
-typedef std::tuple<mfmg::DealIIMeshEvaluator<2>,
-                   mfmg::DealIIMatrixFreeMeshEvaluator<2>>
-    mesh_evaluator_types;
-BOOST_AUTO_TEST_CASE_TEMPLATE(benchmark, MeshEvaluator, mesh_evaluator_types)
+BOOST_AUTO_TEST_CASE(benchmark)
 {
   auto params = std::make_shared<boost::property_tree::ptree>();
   boost::property_tree::info_parser::read_info("hierarchy_input.info", *params);
-  if (mfmg::is_matrix_free<MeshEvaluator>::value)
-  {
-    params->put("smoother.type", "Chebyshev");
-  }
 
-  test<MeshEvaluator>(params);
+  test<mfmg::DealIIMeshEvaluator<2>>(params);
 }
 
+BOOST_AUTO_TEST_CASE(benchmark_mf)
+{
+  auto params = std::make_shared<boost::property_tree::ptree>();
+  boost::property_tree::info_parser::read_info("hierarchy_input.info", *params);
+  params->put("smoother.type", "Chebyshev");
+
+  test_mf<mfmg::DealIIMatrixFreeMeshEvaluator<2>>(params);
+}
+
+typedef std::tuple<mfmg::DealIIMeshEvaluator<2>,
+                   mfmg::DealIIMatrixFreeMeshEvaluator<2>>
+    mesh_evaluator_types;
 BOOST_AUTO_TEST_CASE_TEMPLATE(ml, MeshEvaluator, mesh_evaluator_types)
 {
   auto params = std::make_shared<boost::property_tree::ptree>();
   boost::property_tree::info_parser::read_info("hierarchy_input.info", *params);
+  bool is_matrix_free = false;
   if (mfmg::is_matrix_free<MeshEvaluator>::value)
   {
     params->put("smoother.type", "Chebyshev");
+    is_matrix_free = true;
   }
 
-  double gold_rate = test<MeshEvaluator>(params);
+  double gold_rate = is_matrix_free ? test_mf<MeshEvaluator>(params)
+                                    : test<MeshEvaluator>(params);
 
   params->put("coarse.type", "ml");
   params->put("coarse.params.smoother: type", "symmetric Gauss-Seidel");
   params->put("coarse.params.max levels", 1);
   params->put("coarse.params.coarse: type", "Amesos-KLU");
 
-  double ml_rate = test<MeshEvaluator>(params);
+  double ml_rate = is_matrix_free ? test_mf<MeshEvaluator>(params)
+                                  : test<MeshEvaluator>(params);
 
   BOOST_TEST(ml_rate == gold_rate, tt::tolerance(1e-9));
 
   params->put("coarse.params.max levels", 2);
 
-  ml_rate = test<MeshEvaluator>(params);
+  ml_rate = is_matrix_free ? test_mf<MeshEvaluator>(params)
+                           : test<MeshEvaluator>(params);
 
   BOOST_TEST(ml_rate > gold_rate);
 }
@@ -190,8 +283,9 @@ BOOST_DATA_TEST_CASE(
     }
 
     double const conv_rate =
-        is_matrix_free ? test<mfmg::DealIIMatrixFreeMeshEvaluator<dim>>(params)
-                       : test<mfmg::DealIIMeshEvaluator<dim>>(params);
+        is_matrix_free
+            ? test_mf<mfmg::DealIIMatrixFreeMeshEvaluator<dim>>(params)
+            : test<mfmg::DealIIMeshEvaluator<dim>>(params);
 
     std::string distort_random_str =
         (distort_random ? "distort" : "no_distort");
@@ -215,21 +309,21 @@ BOOST_DATA_TEST_CASE(
     ref_solution[std::make_tuple("hyper_ball" , "distort"    , "None"                  , "lapack"  , "matrix-full")] = 0.1024334788;
     ref_solution[std::make_tuple("hyper_ball" , "distort"    , "Reverse Cuthill_McKee" , "lapack"  , "matrix-full")] = 0.1024334788;
     ref_solution[std::make_tuple("hyper_cube" , "no_distort" , "None"                  , "lanczos" , "matrix-full")] = 0.0235237332;
-    ref_solution[std::make_tuple("hyper_cube" , "no_distort" , "None"                  , "lanczos" , "matrix-free")] = 0.0880046389;
+    ref_solution[std::make_tuple("hyper_cube" , "no_distort" , "None"                  , "lanczos" , "matrix-free")] = 0.0880045475;
     ref_solution[std::make_tuple("hyper_cube" , "no_distort" , "Reverse Cuthill_McKee" , "lanczos" , "matrix-full")] = 0.0235237332;
-    ref_solution[std::make_tuple("hyper_cube" , "no_distort" , "Reverse Cuthill_McKee" , "lanczos" , "matrix-free")] = 0.0880046389;
+    ref_solution[std::make_tuple("hyper_cube" , "no_distort" , "Reverse Cuthill_McKee" , "lanczos" , "matrix-free")] = 0.0880045475;
     ref_solution[std::make_tuple("hyper_cube" , "distort"    , "None"                  , "lanczos" , "matrix-full")] = 0.0220847463;
     ref_solution[std::make_tuple("hyper_cube" , "distort"    , "None"                  , "lanczos" , "matrix-free")] = 0.0859227356;
     ref_solution[std::make_tuple("hyper_cube" , "distort"    , "Reverse Cuthill_McKee" , "lanczos" , "matrix-full")] = 0.0220847463;
     ref_solution[std::make_tuple("hyper_cube" , "distort"    , "Reverse Cuthill_McKee" , "lanczos" , "matrix-free")] = 0.0859227356;
     ref_solution[std::make_tuple("hyper_ball" , "no_distort" , "None"                  , "lanczos" , "matrix-full")] = 0.1148148381;
-    ref_solution[std::make_tuple("hyper_ball" , "no_distort" , "None"                  , "lanczos" , "matrix-free")] = 0.3012330588;
+    ref_solution[std::make_tuple("hyper_ball" , "no_distort" , "None"                  , "lanczos" , "matrix-free")] = 0.2981146185;
     ref_solution[std::make_tuple("hyper_ball" , "no_distort" , "Reverse Cuthill_McKee" , "lanczos" , "matrix-full")] = 0.1148148381;
-    ref_solution[std::make_tuple("hyper_ball" , "no_distort" , "Reverse Cuthill_McKee" , "lanczos" , "matrix-free")] = 0.3012330588;
+    ref_solution[std::make_tuple("hyper_ball" , "no_distort" , "Reverse Cuthill_McKee" , "lanczos" , "matrix-free")] = 0.2981146185;
     ref_solution[std::make_tuple("hyper_ball" , "distort"    , "None"                  , "lanczos" , "matrix-full")] = 0.1024523994;
-    ref_solution[std::make_tuple("hyper_ball" , "distort"    , "None"                  , "lanczos" , "matrix-free")] = 0.2977974869;
+    ref_solution[std::make_tuple("hyper_ball" , "distort"    , "None"                  , "lanczos" , "matrix-free")] = 0.2990825376;
     ref_solution[std::make_tuple("hyper_ball" , "distort"    , "Reverse Cuthill_McKee" , "lanczos" , "matrix-full")] = 0.1024523994;
-    ref_solution[std::make_tuple("hyper_ball" , "distort"    , "Reverse Cuthill_McKee" , "lanczos" , "matrix-free")] = 0.2977974869;
+    ref_solution[std::make_tuple("hyper_ball" , "distort"    , "Reverse Cuthill_McKee" , "lanczos" , "matrix-free")] = 0.2990825376;
     // clang-format on
 
     BOOST_TEST(
@@ -259,8 +353,9 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(zoltan, MeshEvaluator, mesh_evaluator_types)
 
     // This is a gold standard test. Not the greatest but it makes sure we don't
     // break the code
-    double const ref_solution = is_matrix_free ? 0.897494709 : 0.838850614;
-    double const conv_rate = test<MeshEvaluator>(params);
+    double const ref_solution = is_matrix_free ? 0.895602663 : 0.836618927;
+    double const conv_rate = is_matrix_free ? test_mf<MeshEvaluator>(params)
+                                            : test<MeshEvaluator>(params);
     BOOST_TEST(conv_rate == ref_solution, tt::tolerance(1e-6));
   }
 }
@@ -445,14 +540,14 @@ BOOST_AUTO_TEST_CASE(fast_multiply_transpose_mf)
 
     // Compute the fast AP
     params->put("fast_ap", true);
-    Laplace<dim, DVector> fast_laplace(comm, 1);
-    fast_laplace.setup_system(laplace_ptree);
-    fast_laplace.assemble_system(source, *material_property);
+    int constexpr fe_degree = 1;
+    LaplaceMatrixFree<dim, fe_degree, double> fast_laplace(comm);
+    fast_laplace.setup_system(laplace_ptree, *material_property);
 
-    auto fast_evaluator = std::make_shared<
-        TestMeshEvaluator<mfmg::DealIIMatrixFreeMeshEvaluator<2>>>(
-        fast_laplace._dof_handler, fast_laplace._constraints, 1,
-        fast_laplace._system_matrix, material_property);
+    auto fast_evaluator =
+        std::make_shared<TestMFMeshEvaluator<2, fe_degree, double>>(
+            fast_laplace._dof_handler, fast_laplace._constraints,
+            fast_laplace._laplace_operator, material_property);
     std::unique_ptr<mfmg::HierarchyHelpers<DVector>> fast_hierarchy_helpers(
         new mfmg::DealIIMatrixFreeHierarchyHelpers<dim, DVector>());
 
